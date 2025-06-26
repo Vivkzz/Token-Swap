4dc711d5d8668fadadffcb2a69ac740eed7f02f9
Vivkzz <tannavivek20@gmail.com>
2025-06-26 16:41:38 +0530

    initial project setup

---
+++ src/TokenA.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenA is ERC20 {
+    constructor() ERC20("Token A", "TKA") {
+        _mint(msg.sender, 1000000);
+    }
+}
---
+++ src/TokenB.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenB is ERC20 {
+    constructor() ERC20("Token B", "TKB") {
+        _mint(msg.sender, 1000000);
+    }
+}
