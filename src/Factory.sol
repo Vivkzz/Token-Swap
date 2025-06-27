0a5642ead146da6738fbcc88b6a577e2386002e7
Vivkzz <tannavivek20@gmail.com>
2025-06-27 16:28:21 +0530

    Factory contract

---
+++ src/Factory.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8;
+
+import {Pair} from "./Pair.sol";
+
+contract Factory {
+    address[] public allPair;
+    mapping(address => mapping(address => address)) public getPair;
+
+    event PairCreated(address indexed token0, address indexed token1, address indexed pair, uint256 length);
+
+    function sortTokens(address tokenA, address tokenB) public pure returns (address token0, address token1) {
+        require(tokenA != tokenB, "both token cant be same");
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "Can't be zero address");
+    }
+
+    function createPair(address token0, address token1) external {
+        (token0, token1) = sortTokens(token0, token1);
+        require(getPair[token0][token1] == address(0), "Pair alreaddy availabe");
+
+        bytes memory bytecode = abi.encodePacked(type(Pair).creationCode, abi.encode(token0, token1));
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+
+        address pair;
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // optional: not needed
+        allPair.push(pair);
+        emit PairCreated(token0, token1, pair, allPair.length);
+    }
+
+    function allPairLength() public view returns (uint256) {
+        return allPair.length;
+    }
+}
